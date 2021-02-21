e323411d0250c266d515587bbffa32fe31bcd441
Marcos Dantas <marcos.adantas@hotmail.com>
2021-02-20 23:24:17 -0300

    Ações agora podem se auto-calcularem.

--- src/domains/stock.rs
+++ src/domains/stock.rs
@@ -3,3 +3,17 @@ pub struct Stock {
     pub price: f32,
     pub qty: u32
 }
+
+impl Stock {
+    pub fn new(name: String, price: f32, qty: u32) -> Self {
+        Stock {
+            name,
+            price,
+            qty
+        }
+    }
+
+    pub fn calculate_total_value(&self) -> f32 {
+        self.price * self.qty as f32
+    }
+}
