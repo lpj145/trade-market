33659d516dd7e467e8792a19ec555dd4673c4aac
Marcos Dantas <marcos.adantas@hotmail.com>
2021-02-20 23:25:40 -0300

    Explicitando de criação dos interessados e ações.

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,27 @@
 mod domains;
 
-use domains::broker::Broker;
+use domains::{
+    broker::Broker,
+    stock::Stock,
+    interested::Interested,
+    broker_details::BrokerDetails
+};
 
 fn main() {
-    print!("Welcome to market share.\n");
     let mut broker = Broker::new();
-    broker.add_stock_list(vec![
-        broker.factory_stock("MGLU3".to_string(), 24.94, 500),
-        broker.factory_stock("PCAR3".to_string(), 64.75, 200),
-        broker.factory_stock("NTCO3".to_string(), 52.55, 100),
-        broker.factory_stock("BIDI11".to_string(), 159.14, 3000),
-        broker.factory_stock("BBAS3".to_string(), 32.63, 500),
-        broker.factory_stock("BBDC4".to_string(), 24.70, 400),
-    ]);
-    broker.print_broker_details();
+    broker
+        .add_stock_list(vec![
+            Stock::new("MGLU3".to_string(), 24.94, 500),
+            Stock::new("PCAR3".to_string(), 64.75, 200),
+            Stock::new("BIDI11".to_string(), 159.14, 3000),
+            Stock::new("NTCO3".to_string(), 52.55, 100),
+            Stock::new("BBAS3".to_string(), 32.63, 500),
+            Stock::new("BBDC4".to_string(), 24.70, 400),
+        ])
+        .add_interested_list(vec![
+            Interested::new("Marcos Dantas".to_string(), 2000.89),
+        ]);
+
+    BrokerDetails::new(&broker)
+        .show_details();
 }
